ab7c32f471b3db0a562aae6ef9df38234d7df32b
VolatileMark <25851052+VolatileMark@users.noreply.github.com>
2023-04-05 14:30:29 +0200

    scripts: Composite midgrounds on top of backgrounds

--- scripts/textures/textures.py
+++ scripts/textures/textures.py
@@ -9,6 +9,20 @@ import numpy as np
 import math
 import qoi
 
+def optional_key(json_data, key, default):
+    if json_data.get(key) is not None:
+        return json_data[key]
+    return default
+
+def require_key(json_data, key):
+    if json_data.get(key) is not None:
+        return json_data[key]
+    print("Cannot find key '{}' in json data".format(key))
+    exit(-1)
+
+def check_key(json_data, key):
+    return json_data.get(key) is not None
+
 def swizzle(in_pixels, width, height):
     bytes_width = width * 4
     row_blocks = int(bytes_width / 16);
@@ -42,7 +56,7 @@ class Vector2:
         self.y = y
 
 class Tile:
-    def __init__(self, image, offset, size, vpad, hpad):
+    def __init__(self, image, offset, size, ypad, xpad):
         self._pixels = []
         for y in range(size.y):
             self._pixels.append([])
@@ -50,8 +64,8 @@ class Tile:
                 self._pixels[y].append([])
                 self._pixels[y][x] = image[offset.y + y][offset.x + x]
         self._size = Vector2(size.x, size.y)
-        self._hpad = hpad
-        self._vpad = vpad
+        self._xpad = xpad
+        self._ypad = ypad
 
     def crop_to(self, new_size):
         horizontal_delta = new_size.x - self._size.x
@@ -70,38 +84,38 @@ class Tile:
         self._size.y = new_size.y
 
     def _grow_horizontally(self, horizontal_delta):
-        if self._hpad == "center":
+        if self._xpad == "center":
             horizontal_delta = round(horizontal_delta / 2)
-        if self._hpad == "left" or self._hpad == "center":
+        if self._xpad == "left" or self._xpad == "center":
             self._pixels = np.insert(self._pixels, [0 for _ in range(horizontal_delta)], [0, 0, 0, 0], axis=1)
-        if self._hpad == "right" or self._hpad == "center":
+        if self._xpad == "right" or self._xpad == "center":
             self._pixels = np.append(self._pixels, [[[0, 0, 0, 0] for _ in range(horizontal_delta)] for _ in range(self._size.y)], axis=1)
     
     def _shrink_horizontally(self, horizontal_delta):
-        if self._hpad == "center":
+        if self._xpad == "center":
             horizontal_delta = round(horizontal_delta / 2)
-        if self._hpad == "left" or self._hpad == "center":
+        if self._xpad == "left" or self._xpad == "center":
             for _ in range(horizontal_delta):
                 self._pixels = np.delete(self._pixels, 0, axis=1)
-        if self._hpad == "right" or self._hpad == "center":
+        if self._xpad == "right" or self._xpad == "center":
             for _ in range(horizontal_delta):
                 self._pixels = np.delete(self._pixels, self._pixels.shape[1] - 1, axis=1)
     
     def _grow_vertically(self, vertical_delta):
-        if self._vpad == "center":
+        if self._ypad == "center":
             vertical_delta = round(vertical_delta / 2)
-        if self._vpad == "top" or self._vpad == "center":
+        if self._ypad == "top" or self._ypad == "center":
             self._pixels = np.insert(self._pixels, [0 for _ in range(vertical_delta)], [[0, 0, 0, 0] for _ in range(self._size.x)], axis=0)
-        if self._vpad == "bottom" or self._vpad == "center":
+        if self._ypad == "bottom" or self._ypad == "center":
             self._pixels = np.append(self._pixels, [[[0, 0, 0, 0] for _ in range(self._size.x)] for _ in range(vertical_delta)], axis=0)
 
     def _shrink_vertically(self, vertical_delta):
-        if self._vpad == "center":
+        if self._ypad == "center":
             vertical_delta = round(vertical_delta / 2)
-        if self._vpad == "top" or self._vpad == "center":
+        if self._ypad == "top" or self._ypad == "center":
             for _ in range(vertical_delta):
                 self._pixels = np.delete(self._pixels, 0, axis=0)
-        if self._vpad == "bottom" or self._vpad == "center":
+        if self._ypad == "bottom" or self._ypad == "center":
             for _ in range(vertical_delta):
                 self._pixels = np.delete(self._pixels, self._pixels.shape[0] - 1, axis=0)
     
@@ -135,13 +149,16 @@ class Tile:
 
 class Tilemap:
     def __init__(self, json_data):
-        self._name = json_data["name"]
-        self._offset = Vector2(json_data["offset"][0], json_data["offset"][1])
-        self._tile_size = Vector2(json_data["tileSize"][0], json_data["tileSize"][1])
-        self._size_in_tiles = Vector2(json_data["sizeInTiles"][0], json_data["sizeInTiles"][1])
-        self._total_tiles = json_data["totalTiles"]
-        self._vertical_padding = json_data["vpad"]
-        self._horizontal_padding = json_data["hpad"]
+        self._name = require_key(json_data, "name")
+        offset = require_key(json_data, "offset")
+        self._offset = Vector2(offset[0], offset[1])
+        tile_size = require_key(json_data, "tileSize")
+        self._tile_size = Vector2(tile_size[0], tile_size[1])
+        size_in_tiles = require_key(json_data, "sizeInTiles")
+        self._size_in_tiles = Vector2(size_in_tiles[0], size_in_tiles[1])
+        self._total_tiles = require_key(json_data, "totalTiles")
+        self._horizontal_padding = require_key(json_data, "xpad")
+        self._vertical_padding = require_key(json_data, "ypad")
     
     def _generate_image(self, tiles, top_left, bottom_right, output_folder, should_swizzle):
         pixels = []
@@ -157,12 +174,15 @@ class Tilemap:
             pixels.extend(tile.get_pixels())
         output_path = output_folder.joinpath(self._name + ".qoi")
         rgba = np.array(pixels, dtype=np.uint8)
-        if should_swizzle == True:
+        if should_swizzle is True:
             rgba = swizzle(rgba.flatten(), new_size.x, new_size.y)
         qoi.write(output_path, rgba)
 
     def extract(self, image, output_folder, should_swizzle):
-        print("  -> extracting {} {} from tilemap '{}' {} swizzle".format(self._total_tiles, "image" if self._total_tiles == 1 else "tiles", self._name, "with" if should_swizzle == True else "without"))
+        print("  -> extracting {} {} from tilemap '{}' {} swizzle".format(
+            self._total_tiles, 
+            "image" if self._total_tiles == 1 else "tiles", 
+            self._name, "with" if should_swizzle == True else "without"))
         tilestrip_images = []
         tiles_read = 0
         top_left = Vector2(self._tile_size.x, self._tile_size.y)
@@ -183,30 +203,58 @@ class Tilemap:
 
 class TextureFile:
     def __init__(self, json_data, input_folder):
-        self._file = json_data["file"]
-        self._swizzle = True
-        if json_data.get("swizzle") is not None:
-            self._swizzle = json_data["swizzle"]
-        self._path = pathlib.Path(input_folder).joinpath(self._file)
+        self._type = optional_key(json_data, "type", "single")
+        self._files = require_key(json_data, "files") if self._type == "composite" else [require_key(json_data, "file")]
+        self._paths = [pathlib.Path(input_folder).joinpath(file) for file in self._files]
+        self._swizzle = optional_key(json_data, "swizzle", True)
         self._tilemaps = []
-        if json_data.get("texture") is not None:
-            texture_data = json_data["texture"]
-            tilemap = { "name": texture_data["name"], "offset": [0, 0], "tileSize": texture_data["size"], "sizeInTiles": [1, 1], "totalTiles": 1, "vpad": "bottom", "hpad": "right" }
+        if self._type == "single" or self._type == "composite":
+            texture_data = require_key(json_data, "texture")
+            tilemap = {
+                "name": texture_data["name"],
+                "offset": [0, 0],
+                "tileSize": texture_data["size"],
+                "sizeInTiles": [1, 1],
+                "totalTiles": 1,
+                "xpad": "right",
+                "ypad": "bottom"
+            }
             self._tilemaps.append(Tilemap(tilemap))
             self._is_texture = True
-        else:
-            for tilemap in json_data["tilemaps"]:
+        elif self._type == "container":
+            tilemaps = require_key(json_data, "tilemaps")
+            for tilemap in tilemaps:
                 self._tilemaps.append(Tilemap(tilemap)) 
             self._is_texture = False
+        else:
+            print("Unknown texture type '{}'", self._type)
+            exit(-1)
     
     def extract_all(self, output_path):
-        print("Extracting image(s) from '{}'".format(self._file))
-        image = iio.imread(self._path, mode="RGBA")
+        print("Extracting {} from file(s): {}".format(
+            "tilemaps" if self._type == "container" else self._type + " texture", 
+            self._files[0] if len(self._files) == 1 else "\n > " + "\n > ".join(self._files)))
+        if self._type == "composite":
+            image = np.copy(iio.imread(self._paths[0]))
+            for i in range(1, len(self._paths)):
+                composite_image = iio.imread(self._paths[i], mode="RGBA")
+                if composite_image.shape != image.shape:
+                    print("Incompatible image sizes:\n -> {} has size {} while {} has size {}".format(
+                        self._files[0], str(image.shape), self._files[i], str(composite_image.shape)))
+                    exit(-1)
+                for y in range(image.shape[0]):
+                    for x in range(image.shape[1]):
+                        pixel_color = image[y][x]
+                        if pixel_color[3] == 0:
+                            pixel_color = composite_image[y][x]
+                            image[y][x] = pixel_color
+        else:
+            image = iio.imread(self._paths[0], mode="RGBA")
         output_folder = pathlib.Path(output_path)
         if not self._is_texture:
-            relative_output = self._file.split(".")[0]
+            relative_output = self._files[0].split(".")[0]
         else:
-            relative_output = os.path.split(self._file)[0]
+            relative_output = os.path.split(self._files[0])[0]
         output_folder = output_folder.joinpath(relative_output)
         if not output_folder.exists():
             output_folder.mkdir(parents=True, exist_ok=True)
@@ -235,10 +283,12 @@ if __name__ == "__main__":
         print("Error: failed to read descriptor file: {}.".format(str(e)))
         exit(-1)
 
-    for file in json_data:
-        texture_path = pathlib.Path(input_folder).joinpath(file["file"])
-        if not texture_path.exists():
-            print("Error: file '{}' does not exist.".format(texture_path))
-            exit(-1)
-        texture_file = TextureFile(file, input_folder)
+    for texture_json in json_data:
+        files = [require_key(texture_json, "file")] if check_key(texture_json, "file") is True else require_key(texture_json, "files")
+        for file in files:
+            texture_path = pathlib.Path(input_folder).joinpath(file)
+            if not texture_path.exists():
+                print("Error: file '{}' does not exist.".format(texture_path))
+                exit(-1)
+        texture_file = TextureFile(texture_json, input_folder)
         texture_file.extract_all(output_folder)
